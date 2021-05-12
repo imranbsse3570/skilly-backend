11d0c6047cf6555500bbaaa75a5649850058dfb0
Imran Munir <imranmunir312@gmail.com>
2021-05-12 17:17:58 +0500

    Adding security header and data sanitization
    Co-Authored-By: nomanabid123 <78290209+nomanabid123@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const courseRouter = require("./router/courseRouter");
 const reviewRouter = require("./router/reviewRouter");
@@ -8,8 +13,28 @@ const globalErrorHandler = require("./controller/errorController");
 
 const app = express();
 
-app.use(express.json());
+app.use(helmet());
+
 if (process.env.NODE_ENV === "DEVELOPMENT") app.use(morgan("dev"));
+
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: ["duration", "price"],
+  })
+);
 
 app.use("/api/v1/courses", courseRouter);
 app.use("/api/v1/reviews", reviewRouter);
