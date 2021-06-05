52ca27c51f5ef5dae5ac599b3527cc66ce2de42f
Imran Munir <imranmunir312@gmail.com>
2021-06-05 16:10:17 +0500

    server updated

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT || 5000;
@@ -23,6 +29,14 @@ mongoose
 
 const app = require("./app");
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening at port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
