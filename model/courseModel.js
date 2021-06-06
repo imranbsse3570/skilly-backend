03a8ce6bf7987198768a2335657729f9116c671d
Imran Munir <imranmunir312@gmail.com>
2021-06-06 16:12:43 +0500

    Ready for deployment

---
+++ model/courseModel.js
@@ -0,0 +1,107 @@
+const mongoose = require("mongoose");
+
+const Quiz = require("./quizModel");
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Course Title is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Course Description is required"],
+  },
+  summary: {
+    type: String,
+    required: [true, "Course Summary is required"],
+  },
+  requirements: {
+    type: String,
+    required: [true, "Course Requirement is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(Date.now()),
+  },
+  noOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  rating: {
+    type: Number,
+    default: 1,
+    min: [1, "Minimum Review must be 1"],
+    max: [5, "Maximum review must be 5"],
+    set: (value) => Math.round(value * 10) / 10,
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+  },
+  isPublished: {
+    type: Boolean,
+    default: false,
+  },
+  languages: [
+    {
+      type: String,
+      enum: [
+        "English",
+        "Urdu",
+        "German",
+        "Dutch",
+        "Italian",
+        "Hindi",
+        "Spanish",
+      ],
+      default: "English",
+    },
+  ],
+  image: {
+    type: String,
+  },
+  totalDuration: {
+    type: Number,
+    default: 0,
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  lectures: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Lecture",
+    },
+  ],
+  category: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Category",
+  },
+  slug: {
+    type: String,
+    unique: true,
+  },
+  quiz: {
+    type: Quiz,
+  },
+});
+
+courseSchema.index({ price: -1 });
+courseSchema.index({ rating: 1 });
+courseSchema.index({ totalDuration: 1 });
+courseSchema.index(
+  { price: -1, rating: 1 },
+  {
+    unique: true,
+  }
+);
+
+courseSchema.pre(/^find/, function (next) {
+  this.populate("author").populate("category").populate("lectures");
+  next();
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+module.exports = Course;
