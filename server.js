856c1d2224e832948158abcab04fae0e6e82bc9f
Imran Munir <imranmunir312@gmail.com>
2021-06-07 16:38:38 +0500

    Updated Sigterm signal
    Co-Authored-By: nomanabid123 <78290209+nomanabid123@users.noreply.github.com>

--- server.js
+++ server.js
@@ -40,3 +40,10 @@ process.on("unhandledRejection", (err) => {
     process.exit(1);
   });
 });
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM recieved Shutting Down Server");
+  server.close(() => {
+    console.log("Shutting Down server");
+  });
+});
