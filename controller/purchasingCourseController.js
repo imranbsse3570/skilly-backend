ac51cca66a8ec1f77524ce37aea8735385f659ee
Imran Munir <imranmunir312@gmail.com>
2021-06-07 16:22:20 +0500

    Adding webhook
    Co-Authored-By: nomanabid123 <78290209+nomanabid123@users.noreply.github.com>

--- controller/purchasingCourseController.js
+++ controller/purchasingCourseController.js
@@ -1,5 +1,16 @@
 const catchAsync = require("../util/catchAsync");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.addCourseInUserPurchaseList = catchAsync(async (req, res, next) => {
-  console.log(req.body);
+  const signature = req.headers["stripe-signature"];
+
+  const event = stripe.webhooks.constructEvents(
+    req.body,
+    signature,
+    process.env.process.env.STRIPE_WEBHOOK_SECRET
+  );
+
+  if (event.type === "checkout.session.completed") {
+    console.log(event.data.object);
+  }
 });
