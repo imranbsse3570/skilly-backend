fdd8ae5e3aac8e3370508deeade7cc79d7479d69
Imran Munir <imranmunir312@gmail.com>
2021-06-08 16:18:12 +0500

    node Engines specifed

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -2,37 +2,41 @@ const nodemailer = require("nodemailer");
 const mg = require("nodemailer-mailgun-transport");
 
 const sendEmail = async (emails, subject, text, html) => {
-  let transporter;
+  try {
+    let transporter;
 
-  if (process.env.NODE_ENV === "production") {
-    const auth = {
-      auth: {
-        api_key: process.env.PRODUCTION_EMAIL_API_KEY,
-        domain: process.env.PRODUCTION_EMAIL_DOMAIN,
-      },
+    if (process.env.NODE_ENV === "production") {
+      const auth = {
+        auth: {
+          api_key: process.env.PRODUCTION_EMAIL_API_KEY,
+          domain: process.env.PRODUCTION_EMAIL_DOMAIN,
+        },
+      };
+
+      transporter = nodemailer.createTransport(mg(auth));
+    } else {
+      transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+    }
+
+    const mailOptions = {
+      from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_OF_SENDER}>`, // sender address
+      to: emails.join(","), // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html, // html body
     };
 
-    transporter = nodemailer.createTransport(mg(auth));
-  } else {
-    transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+    await transporter.sendMail(mailOptions);
+  } catch (err) {
+    console.log(err);
   }
-
-  const mailOptions = {
-    from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_OF_SENDER}>`, // sender address
-    to: emails.join(","), // list of receivers
-    subject, // Subject line
-    text, // plain text body
-    html, // html body
-  };
-
-  await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
