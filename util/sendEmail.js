49d6230f458126aa0a9f36fa36473997602f09ce
Imran Munir <imranmunir312@gmail.com>
2021-06-08 15:07:26 +0500

    Added Production Mail service

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -1,14 +1,28 @@
 const nodemailer = require("nodemailer");
+const mg = require("nodemailer-mailgun-transport");
 
 const sendEmail = async (emails, subject, text, html) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  let transporter;
+
+  if (process.env.NODE_ENV === "production") {
+    const auth = {
+      auth: {
+        api_key: process.env.PRODUCTION_EMAIL_API_KEY,
+        domain: process.env.PRODUCTION_EMAIL_DOMAIN,
+      },
+    };
+
+    transporter = nodemailer.createTransport(mg(auth));
+  } else {
+    transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
   const mailOptions = {
     from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_OF_SENDER}>`, // sender address
