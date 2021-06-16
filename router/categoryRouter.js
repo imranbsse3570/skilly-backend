d10ff7b295de54b70d33f5cec10fb73835fdab9c
Imran Munir <imranmunir312@gmail.com>
2021-06-16 23:50:52 +0500

    changed slug
    Co-Authored-By: Noman Abid <78290209+nomanabid123@users.noreply.github.com>

--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -10,8 +10,6 @@ const router = express.Router();
 
 router.get("/", factoryController.getAll(Category));
 
-router.get("/get/:slug", factoryController.getOneBySlug(Category));
-
 router.get("/:id", factoryController.getOne(Category));
 
 router.use(authController.protect);
--- util/generateUniqueSlug.js
+++ util/generateUniqueSlug.js
@@ -3,7 +3,7 @@ const slugify = require("slugify");
 module.exports = (resource, title) => {
   const slug = slugify(title, {
     lower: true,
-    replacement: "-",
+    replacement: "_",
   });
 
   if (resource.length > 0) {
