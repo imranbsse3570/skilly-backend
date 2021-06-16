ae6fb43040ae93d313ec66623cc4fc678f872546
Imran Munir <imranmunir312@gmail.com>
2021-06-16 19:12:59 +0500

    virtuals added
    Co-Authored-By: Noman Abid <78290209+nomanabid123@users.noreply.github.com>

--- model/courseCategoryModel.js
+++ model/courseCategoryModel.js
@@ -1,19 +1,25 @@
 const mongoose = require("mongoose");
 
-const courseCategorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title of the Category is required"],
+const courseCategorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title of the Category is required"],
+    },
+    slug: {
+      type: String,
+      unique: [true, "Slug must be unique"],
+    },
+    previewImage: {
+      type: String,
+      default: "default.jpg",
+    },
   },
-  slug: {
-    type: String,
-    unique: [true, "Slug must be unique"],
-  },
-  previewImage: {
-    type: String,
-    default: "default.jpg",
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 courseCategorySchema.virtual("courses", {
   ref: "Course",
