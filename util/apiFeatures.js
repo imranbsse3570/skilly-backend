f825b65ed3a01e78a72a445e3bf8ea2846b15afb
Imran Munir <imranmunir312@gmail.com>
2021-06-24 16:35:55 +0500

    Adding eq and ne to filtering
    Co-Authored-By: Noman Abid <78290209+nomanabid123@users.noreply.github.com>

--- util/apiFeatures.js
+++ util/apiFeatures.js
@@ -11,7 +11,10 @@ class APIFeatures {
 
     // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lte|lt|ne|eq)\b/g,
+      (match) => `$${match}`
+    );
 
     this.query = this.query.find(JSON.parse(queryStr));
 
