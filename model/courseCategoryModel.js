f3fcbe7481d6c9731f29d1b8d17f39b1267ab269
Imran Munir <imranmunir312@gmail.com>
2021-06-25 16:00:13 +0500

    Depreciation Warning Removed
    Co-Authored-By: Noman Abid <78290209+nomanabid123@users.noreply.github.com>

--- model/courseCategoryModel.js
+++ model/courseCategoryModel.js
@@ -28,7 +28,7 @@ courseCategorySchema.virtual("courses", {
 });
 
 courseCategorySchema.statics.random = async function () {
-  const count = await this.count();
+  const count = await this.countDocuments();
   const rand = Math.floor(Math.random() * count);
   const randomDoc = await this.findOne().skip(rand).populate("courses");
   return randomDoc;
